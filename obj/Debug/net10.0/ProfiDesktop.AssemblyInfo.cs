d7fa3dde8ba5e4066b2054904431e4af7f77e94c
kukur1k <apurkin97@gmail.com>
2026-08-20 22:13:56 +0300

    add delete btn (zaglushka)

--- obj/Debug/net10.0/ProfiDesktop.AssemblyInfo.cs
+++ obj/Debug/net10.0/ProfiDesktop.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProfiDesktop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+820c6cbf596ac26866ce78661fb4ee979a9278bd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40811a5b24ac502e065f965b1e5023f39025cf36")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProfiDesktop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProfiDesktop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
